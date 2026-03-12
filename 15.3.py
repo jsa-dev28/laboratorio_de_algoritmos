83e41aa4905f95f1f159b3a474a0e793b8e159a8
jsa-dev28 <jshlamovitzalfonso@alumno.huergo.edu.ar>
2026-03-12 15:48:20 -0300

    Update 15.3.py

--- 15.3.py
+++ 15.3.py
@@ -1,4 +1,3 @@
-#mejorá el código del ejercicio 15.3 reemplazando la serie de llamadas a print() por un loop que pase por todas las claves y valores del diccionario. Cuando estés seguro de que el loop funciona, agregá cinco términos más de Python a tu glosario. Cuando vuelvas a ejecutar el programa, estas nuevas palabras y sus significados deberían aparecer automáticamente en la salida.
 glosario = {
     "string" : "cadena de caracteres",
     "integer": "número entero",
